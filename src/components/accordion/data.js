da305222d395c5fee3a526b3ca84942ccc780b84
ms.viktoria <melnikviktorialeo@gmail.com>
2024-11-20 21:35:42 +0200

    Accordion project

---
+++ src/components/accordion/data.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    id: '1',
+    question: "What are accordion components?",
+    answer: "Accordion components are UI elements used to show and hide sections of content. They allow users to expand or collapse content panels, making it easier to manage and display information dynamically."
+  },
+  {
+    id: '2',
+    question: "What are they used for?",
+    answer: "Accordion components are used to save screen space and organize content by letting users expand only the information they need while keeping the rest hidden. They're commonly found in FAQs, menus, and forms."
+  },
+  {
+    id: '3',
+    question: "Accordion as a musical instrument?",
+    answer: "An accordion is a musical instrument that produces sound by air flowing over reeds. It is played by compressing and expanding the bellows while pressing buttons or keys to create melodies and harmonies."
+  },
+  {
+    id: '4',
+    question: "Can I create an accordion component with a different framework?",
+    answer: "Yes, you can create accordion components using various frameworks like React, Angular, Vue, or even plain HTML, CSS, and JavaScript. Most frameworks offer libraries or built-in support to simplify implementation."
+  },
+];
+
+export default data;
