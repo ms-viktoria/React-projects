19140b24e20178612519f4722bbe2e973ee5cf7e
ms.viktoria <melnikviktorialeo@gmail.com>
2024-11-22 21:35:18 +0200

    Star rating. Image slider. Load more products component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,44 @@
 import "./App.css";
 import Accordion from "./components/accordion";
+import ImageSlider from "./components/image-slider";
+import LoadMoreData from "./components/load-more-button";
 import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
 
 function App() {
   return (
     <div className="App">
-      {/* {Accordion Component} */}
-      <Accordion />
+      <div className="project">
+        {/* {Accordion component} */}
+        <Accordion />
+      </div>
 
-      {/* {Random cColor component} */}
-      <RandomColor />
-      
+      <div className="project">
+        {/* {Random color component} */}
+        <RandomColor />
+      </div>
+
+      <div className="project">
+        {/* {Star rating component} */}
+        <StarRating noOfStars={10} />
+      </div>
+
+      <div className="project">
+        {/* {Image slider component} */}
+        <ImageSlider
+          url={"https://picsum.photos/v2/list"}
+          page={"1"}
+          limit={"10"}
+        />
+        </div>
+
+        <div className="project">
+        {/* {Load more products component} */}
+        <LoadMoreData />
+        </div>
+    
     </div>
+    
   );
 }
 
--- src/components/star-rating/index.jsx
+++ src/components/star-rating/index.jsx
@@ -1,26 +1,39 @@
-import { useState } from "react"
-import { FaStar } from "react-icons/fa"
+import { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import './styles.css';
 
-
-
-export default function StarRating({noOfStars = 5}){
-
+export default function StarRating({ noOfStars = 5 }) {
   const [rating, setRating] = useState(0);
-  const [hover,setHover] = useState(0);
+  const [hover, setHover] = useState(0);
 
-  function handle
+  function handleClick(getCurrentIndex) {
+    setRating(getCurrentIndex);
+  }
 
-  return <div className="star-rating">
-    {
-      [...Array(noOfStars)].map((_, index) => {
-        return <FaStar
-        key={index}
-        onClick={}
-        onMouseMove={}
-        onMouseLeave={}
-        size={40}
-        />
-      })
-    }
-  </div>
+  function handleClickMouseEnter(getCurrentIndex) {
+    setHover(getCurrentIndex);
+  }
+
+  function handleClickMouseLeave() {
+    setHover(rating);
+  }
+
+  return (
+    <div className="star-rating">
+      {[...Array(noOfStars)].map((_, index) => {
+        index += 1;
+
+        return (
+          <FaStar
+            key={index}
+            className={index <= (hover || rating) ? "active" : "inactive"}
+            onClick={() => handleClick(index)}
+            onMouseMove={() => handleClickMouseEnter(index)}
+            onMouseLeave={() => handleClickMouseLeave()}
+            size={40}
+          />
+        );
+      })}
+    </div>
+  );
 }
